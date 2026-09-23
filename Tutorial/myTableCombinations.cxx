b4101565d9c73850752e6175a97d1473861bfeb2
silasditters03 <fam.ditters@gmail.com>
2026-09-23 22:26:36 +0200

    update tutorial

--- Tutorial/myTableCombinations.cxx
+++ Tutorial/myTableCombinations.cxx
@@ -1,5 +1,5 @@
 #include "Framework/AnalysisTask.h"
-#include "Framework/runDataProcessing.h" // Provides the workflow hooks
+#include "Framework/runDataProcessing.h"
 #include <TH1F.h>
 #include <TFile.h>
 #include <cmath>
@@ -11,23 +11,20 @@ struct MyDeltaPhiTask {
     TH1F* hDeltaPhi = nullptr;
     TFile* outFile = nullptr;
 
-    // 1. Initialize the file and histogram directly
     void init(InitContext& ic) {
         outFile = new TFile("DeltaPhiResults.root", "RECREATE");
         hDeltaPhi = new TH1F("hDeltaPhi", "Delta-Phi Distribution;#Delta#phi;Counts", 100, -M_PI, M_PI);
     }
 
-    // 2. Process the tracks using C++ iterators instead of array subscripts
     void process(aod::Tracks const& tracks) {
-        // Loop over the track table safely
         for (auto it1 = tracks.begin(); it1 != tracks.end(); ++it1) {
             auto it2 = it1;
-            ++it2; // Start inner loop one element ahead to avoid self-pairing
+            ++it2; 
             
             for (; it2 != tracks.end(); ++it2) {
-                float dphi = it1->phi() - it2->phi();
+                // Use dot operator for O2 table iterators
+                float dphi = it1.phi() - it2.phi();
                 
-                // Normalize angle to [-pi, pi]
                 while (dphi > M_PI) dphi -= 2 * M_PI;
                 while (dphi < -M_PI) dphi += 2 * M_PI;
                 
@@ -36,7 +33,6 @@ struct MyDeltaPhiTask {
         }
     }
 
-    // 3. Save and close the file when the data stream ends
     void endOfStream(EndOfStreamContext& ec) {
         if (outFile) {
             outFile->cd();
@@ -46,7 +42,6 @@ struct MyDeltaPhiTask {
     }
 };
 
-// 4. Hook the task into the O2 workflow explicitly (replaces WORKFLOW_APP)
 WorkflowSpec defineDataProcessing(ConfigContext const& cfg) {
     return WorkflowSpec{
         adaptAnalysisTask<MyDeltaPhiTask>(cfg)
